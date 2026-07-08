ae1f900ef1d6070c13c5735a86b6130b0ee4fd2f
Yash Arya <Aryayash904@gmail.com>
2026-07-08 21:58:04 +0530

    doing some hard combo of recursion

---
+++ Recursion/Recursion patterwise/Hard combos/palindrome partioning.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isPalindrome(string &s,int left,int right){
+    while(left<right){
+        if(s[left]!=s[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+void solve(int index,string &s,vector<string>&ds,vector<vector<string>>&ans){
+    if(index==s.size()){
+        ans.push_back(ds);
+        return;
+    }
+    for(int i=index;i<s.size();i++){
+        if(isPalindrome(s,index,i)){
+            ds.push_back(s.substr(index,i-index+1));
+            solve(i+1,s,ds,ans);
+            ds.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string s){
+    vector<vector<string>>ans;
+    vector<string>ds;
+    solve(0,s,ds,ans);
+    return ans;
+}
+
+int main(){
+    string s;
+    cin>>s;
+    vector<vector<string>>ans = partition(s);
+    for(auto i:ans){
+        for(auto j:i){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ Recursion/Recursion patterwise/Hard combos/word check.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool dfs(vector<vector<char>>& board, string word, int row,int col,int index){
+    if(index==word.size()) return true;
+    if(row<0 || row>=board.size() || col<0 || col>= board[0].size() || board[row][col]!=word[index]) return false;
+    char temp = board[row][col];
+    board[row][col] = '*';
+    char found = dfs(board,word,row+1,col,index+1) || dfs(board,word,row-1,col,index+1) || dfs(board,word,row,col+1,index+1) || dfs(board,word,row,col-1,index+1);
+    board[row][col] = temp;
+    return found;
+}
+
+bool exist(vector<vector<char>>& board,string word){
+    for(int i=0;i<board.size();i++){
+        for(int j = 0;j<board[0].size();j++){
+            if(dfs(board,word,i,j,0)) return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    vector<vector<char>> board = {
+        {'A','B','C','E'},
+        {'S','F','C','S'},
+        {'A','D','E','E'}
+    };
+    cout << boolalpha << exist(board, "ABCCED") << endl;
+    cout << boolalpha << exist(board, "SEE") << endl;
+    cout << boolalpha << exist(board, "ABCB") << endl;
+}
